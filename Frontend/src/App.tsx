be56a72e9c33dba54b6d037cad0708cbbb9b946d
Andrinat <Nursultan2000@yandex.ru>
2025-01-17 15:34:38 +0500

    test husky finally

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the sdf Vite and React logos to learn more</p>
     </>
   );
 };
